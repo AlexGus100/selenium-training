eea57adb62c70d89bd1cfe1707ff1845f99b1ef8
AlexGus <aa.guselnikov100@gmail.com>
2024-04-12 04:00:37 +0400

    Task-11: add tests for user sign in

---
+++ Selenium-training/src/test/java/NewUserSignInTest.java
@@ -0,0 +1,62 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class NewUserSignInTest {
+    private WebDriver driver;
+    @BeforeEach
+    public void start(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void checkNewUserSignInTest() {
+        openMainPage();
+
+        driver.findElement(By.cssSelector("a[href='http://localhost/litecart/en/create_account']")).click();
+
+        driver.findElement(By.name("firstname")).sendKeys("Bill");
+        driver.findElement(By.name("lastname")).sendKeys("Smith");
+        driver.findElement(By.name("address1")).sendKeys("Wall Street 12/10");
+        driver.findElement(By.name("postcode")).sendKeys("12345");
+        driver.findElement(By.name("city")).sendKeys("New York");
+        Select country = new Select(driver.findElement(By.name("country_code")));
+        country.selectByVisibleText("United States");
+        Select zoneCode = new Select(driver.findElement(By.cssSelector("select[name=zone_code]")));
+        zoneCode.selectByValue("NY");
+        String userEmail = emailGenerator();
+        driver.findElement(By.name("email")).sendKeys(userEmail);
+        driver.findElement(By.name("phone")).sendKeys("+99999999999");
+        driver.findElement(By.name("password")).sendKeys(userPassword);
+        driver.findElement(By.name("confirmed_password")).sendKeys(userPassword);
+        driver.findElement(By.name("create_account")).click();
+
+        driver.findElement(By.cssSelector("a[href='http://localhost/litecart/en/logout']")).click();
+        driver.findElement(By.name("email")).sendKeys(userEmail);
+        driver.findElement(By.name("password")).sendKeys(userPassword);
+        driver.findElement(By.name("login")).click();
+        driver.findElement(By.cssSelector("a[href='http://localhost/litecart/en/logout']")).click();
+    }
+
+    @AfterEach
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+
+    public void openMainPage() {
+        driver.get("http://localhost/litecart/");
+    }
+
+    public String emailGenerator() {
+        String generatedString = RandomStringUtils.randomAlphabetic(10).toLowerCase();
+        return generatedString + "@gmail.com";
+    }
+
+    final String userPassword = "user";
+}
